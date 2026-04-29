d97cee8a6b74e62d8194acfc40f43990459c0646
realsamrmeier <samrmeier@gmail.com>
2026-04-28 22:16:44 -0700

    Fixed my complex to power
    added fft outputs to pipeline, and turns out vivado fft outputs signed so had to fix that

--- EE271Project.srcs/sources_1/ComplexToPower.v
+++ EE271Project.srcs/sources_1/ComplexToPower.v
@@ -1,24 +1,28 @@
 module ComplexToPower (
-    input wire [31:0] in,
-    output reg[32:0] power,
-    input wire clk
+    input wire [63:0] fft_tdata, //note this is assuming vivado fft block in unscaled mode, lmk if we use scaled 
+    input wire fft_tvalid,
+    output reg[26:0] mag, //also changes if we use scaled, this is for unscaled
+    output reg mag_valid,
+    input wire clk,
+    input wire reset
 );
-    wire [15:0] re;
-    wire [15:0] im;
+    wire [26:0] re = fft_tdata[26:0];
+    wire [26:0] im = fft_tdata[58:32];
 
-    reg [31:0] re_sq, im_sq;
+    wire [26:0] abs_re = re[26] ? -re : re; //turns out fft output is in 2's complement, so we need to take the absolute value
+    wire [26:0] abs_im = im[26] ? -im : im;
 
-    assign re = in[31:16];
-    assign im = in[15:0];
 
-    always @(posedge clk) begin
-        re_sq <= re * re;
-        im_sq <= im * im;
+     always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            mag       <= 0;
+            mag_valid <= 0;
+        end else begin
+            mag       <= (abs_re > abs_im) ? abs_re : abs_im;
+            mag_valid <= fft_tvalid;
+        end
     end
 
-    always @(posedge clk) begin
-        power <= re_sq + im_sq;
-    end
 
 
 endmodule
