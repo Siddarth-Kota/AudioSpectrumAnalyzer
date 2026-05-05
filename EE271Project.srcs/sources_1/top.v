b4b46efc8c348194391079347dcca0bf305f9c3c
Karlo Godfrey Escalona Gregorio <71238603+T3WI@users.noreply.github.com>
2026-05-04 19:59:56 -0700

    Update top.v

--- EE271Project.srcs/sources_1/top.v
+++ EE271Project.srcs/sources_1/top.v
@@ -20,9 +20,11 @@ module top(
     );
     
     wire clk_mic_raw;
+    wire clk_vga;
     reg clk_mic_actual;
     clk_wiz_0 clk0(
         .clk_mic(clk_mic_raw),  // 6 MHz CLK 
+        .clk_vga(clk_vga),
         .reset(RESET),
         .locked(),
         .clk_in1(fclk)
@@ -54,6 +56,7 @@ module top(
 
     wire [23:0] fir_dataout;
     wire        fir_mvalid, fir_mready;
+    assign fir_ready = 1;               // assume window is always ready to accept data
     fir f(
         .data_out(fir_dataout),
         .data_in(cic_dataout),
@@ -64,6 +67,47 @@ module top(
         .clk(M_CLK),
         .reset(RESET)
     );
+
+    wire [23:0] win_data_out;
+    wire        win_mvalid;
+    Window win(
+        .clk(M_CLK),
+        .rst_n(!RESET),
+        .audio_data_in(fir_dataout),
+        .data_valid_in(fir_mvalid),
+        .windowed_data_out(win_data_out),
+        .data_valid_out(win_mvalid)
+    );
+
+    reg [9:0] win_last_count;    
+    always @(posedge M_CLK, posedge RESET) begin 
+        if(RESET) begin 
+            win_last_count <= 0;
+        end
+        else if (win_mvalid) begin 
+            if(win_last_count == 10'd1023) win_last_count <= 0;
+            else win_last_count <= win_last_count + 1;
+        end
+    end
+    wire win_last;
+    assign win_last = win_last_count == 10'd1023;
+    wire        fifo_mvalid, fifo_mready;
+    wire [23:0] fifo_data_out;
+    wire        fifo_mlast;
+    axis_data_fifo_0 axis_fifo(
+        .s_axis_aresetn(!RESET),
+        .s_axis_aclk(M_CLK),
+        .s_axis_tvalid(win_mvalid),
+        .s_axis_tready(1'b1),   // assume FIFO is always ready, may need to fix if FFT stalls
+        .s_axis_tdata(win_data_out),
+        .s_axis_tlast(win_last),
+        .m_axis_tvalid(fifo_mvalid),
+        .m_axis_tready(fifo_mready),
+        .m_axis_tdata(fifo_data_out),
+        .m_axis_tlast(fifo_mlast)
+    );
+    
+    
     //ila_0 i(
         //.clk(M_CLK),
         
